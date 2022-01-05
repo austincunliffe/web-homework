470fbe11739d521f7279fb7431af74917b47851d
austincunliffe <austinmcunliffe@gmail.com>
2022-01-05 06:24:08 -0700

    initial user seed

--- elixir/priv/repo/seeds.exs
+++ elixir/priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Homework.Repo
+alias Homework.Merchants.Merchant
+alias Homework.Transactions.Transaction
+alias Homework.Users.User
+
+Repo.delete_all Merchant
+Repo.delete_all Transaction
+Repo.delete_all User
+
+num_users = 5
+
+Enum.each(0..num_users, fn _n ->
+  %User{
+    first_name: Faker.Person.first_name,
+    last_name: Faker.Person.last_name,
+    dob: Faker.Date.date_of_birth(18..99) |> Date.to_string
+  } |> Repo.insert! end)
