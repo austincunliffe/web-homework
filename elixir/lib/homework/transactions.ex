f7b73bf71c41dbb69d8303ca283152dc07309642
austincunliffe <austinmcunliffe@gmail.com>
2022-01-22 15:26:02 -0700

    handle impact on company available_credit

--- elixir/lib/homework/transactions.ex
+++ elixir/lib/homework/transactions.ex
@@ -4,9 +4,11 @@ defmodule Homework.Transactions do
   """
 
   import Ecto.Query, warn: false
+  alias Ecto.Multi
   alias Homework.Repo
 
   alias Homework.Transactions.Transaction
+  alias Homework.Companies
 
   @doc """
   Returns the list of transactions.
@@ -50,9 +52,17 @@ defmodule Homework.Transactions do
 
   """
   def create_transaction(attrs \\ %{}) do
-    %Transaction{}
-    |> Transaction.changeset(attrs)
-    |> Repo.insert()
+    multi = Multi.new()
+    |> Multi.insert(:insert_transaction, %Transaction{} |> Transaction.changeset(attrs))
+    |> Multi.run(:update_company_available_credit_step, update_company_available_credit())
+
+    case Repo.transaction(multi) do
+      {:ok, %{transaction: transaction}} ->
+        {:ok, transaction}
+
+      error -> {:error, error}
+    end
+
   end
 
   @doc """
@@ -68,9 +78,17 @@ defmodule Homework.Transactions do
 
   """
   def update_transaction(%Transaction{} = transaction, attrs) do
-    transaction
-    |> Transaction.changeset(attrs)
-    |> Repo.update()
+    transaction_changeset = change_transaction(transaction, attrs)
+    multi = Multi.new()
+    |> Multi.update(:update_transaction_step, transaction_changeset)
+    |> Multi.run(:update_company_available_credit_step, update_company_available_credit(transaction_changeset))
+
+    case Repo.transaction(multi) do
+      {:ok, %{transaction: transaction}} ->
+        {:ok, transaction}
+
+      error -> {:error, error}
+    end
   end
 
   @doc """
@@ -86,7 +104,16 @@ defmodule Homework.Transactions do
 
   """
   def delete_transaction(%Transaction{} = transaction) do
-    Repo.delete(transaction)
+    multi = Multi.new()
+    |> Multi.delete(:delete_transaction, transaction)
+    |> Multi.run(:update_company_available_credit_step, update_company_available_credit(transaction))
+
+    case Repo.transaction(multi) do
+      {:ok, %{transaction: transaction}} ->
+        {:ok, transaction}
+
+      error -> {:error, error}
+    end
   end
 
   @doc """
@@ -100,5 +127,59 @@ defmodule Homework.Transactions do
   """
   def change_transaction(%Transaction{} = transaction, attrs \\ %{}) do
     Transaction.changeset(transaction, attrs)
+  end
+
+  defp update_company_available_credit() do
+    fn repo, %{insert_transaction: %{ok: transaction}} ->
+      company = Companies.get_and_lock_company!(transaction.company_id)
+
+      available_credit_adjustment = if transaction.debit, do: -transaction.amount, else: transaction.amount
+      updated_available_credit = company.available_credit + available_credit_adjustment
+
+      company
+      |> Companies.change_company(%{available_credit: updated_available_credit})
+      |> repo.update()
+    end
+  end
+
+  defp update_company_available_credit(transaction) do
+    fn repo, _ ->
+      company = Companies.get_and_lock_company!(transaction.company_id)
+
+      available_credit_adjustment = if transaction.debit, do: -transaction.amount, else: transaction.amount
+      updated_available_credit = company.available_credit + available_credit_adjustment
+
+      company
+      |> Companies.change_company(%{available_credit: updated_available_credit})
+      |> repo.update()
+    end
+  end
+
+  defp update_company_available_credit(%{data: original_data, changes: changes} = _changeset) do
+    fn repo, %{update_transaction_step: %{ok: transaction}} ->
+      company = Companies.get_and_lock_company!(original_data.company_id)
+
+      updated_available_credit = company.available_credit
+      |> reverse_transaction_amount_from_available_credit(original_data, changes)
+      |> update_available_credit_for_new_transaction(transaction)
+
+      company
+      |> Companies.change_company(%{available_credit: updated_available_credit})
+      |> repo.update()
+    end
+  end
+
+  defp reverse_transaction_amount_from_available_credit(available_credit, original_transaction, transaction_changes) do
+    if Enum.any?(["company_id", "debit", "credit", "amount"], &Map.has_key?(transaction_changes, &1)) do
+      available_credit_adjustment = if original_transaction.debit, do: original_transaction.amount, else: -original_transaction.amount
+      ^available_credit = available_credit + available_credit_adjustment
+    end
+    available_credit
+  end
+
+  defp update_available_credit_for_new_transaction(available_credit, transaction) do
+    available_credit_adjustment = if transaction.debit, do: -transaction.amount, else: transaction.amount
+    available_credit = available_credit + available_credit_adjustment
+    available_credit
   end
 end
